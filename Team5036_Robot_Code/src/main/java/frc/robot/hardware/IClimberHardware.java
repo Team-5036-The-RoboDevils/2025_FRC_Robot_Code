ce8b71ff11c6f11ca1fccea31426d214d879ed5c
Shinykittens786 <sherry.ahmad@student.tdsb.on.ca>
2025-02-19 00:26:13 -0500

    adding IClimberHardware because it told me i had unsaved changes to it

--- Team5036_Robot_Code/src/main/java/frc/robot/hardware/IClimberHardware.java
+++ Team5036_Robot_Code/src/main/java/frc/robot/hardware/IClimberHardware.java
@@ -1,9 +1,11 @@
 package frc.robot.hardware;
 
+import edu.wpi.first.units.measure.Angle;
+
 public interface IClimberHardware {
     public void setClimberMotorPower(double val); 
 
     public double getClimberMotorPos(); 
 
-    public void setClimberEncoderPos(double val); 
+    public void zeroClimberEncoderPos(double val); 
 }
