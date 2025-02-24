36ab77b129f3e268cc1204b673a8c15c6ef537dc
Sheikh A. Tahmid <sa2tahmi@uwaterloo.ca>
2025-02-23 22:32:35 -0500

    fixed reference to Encoder DIO port constants, got rid of unnecessary imports

--- Team5036_Robot_Code/src/main/java/frc/robot/hardware/CoralMechanismHardware.java
+++ Team5036_Robot_Code/src/main/java/frc/robot/hardware/CoralMechanismHardware.java
@@ -3,12 +3,6 @@ package frc.robot.hardware;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Encoder; 
 
 import frc.robot.RobotMap;
@@ -21,8 +15,7 @@ public class CoralMechanismHardware implements ICoralMechanismHardware {
     public CoralMechanismHardware() {
         shooterMotor = new TalonSRX(RobotMap.CORALINTAKE_ROLLERS_CAN_ID);
         pivotMotor = new VictorSPX(RobotMap.CORALINTAKE_PIVOT_CAN_ID);
-        pivotCoralEncoder = new Encoder(RobotMap.DIOPortACoralEncoder, RobotMap.DIOPortBCoralEncoder, false, Encoder.EncodingType.k4X); // make these constants uppercase
-
+        pivotCoralEncoder = new Encoder(RobotMap.DIO_PORT_A_CORAL_ENCODER, RobotMap.DIO_PORT_B_CORAL_ENCODER, false, Encoder.EncodingType.k4X); // make these constants uppercase
     }
 
     @Override
